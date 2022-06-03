6873c9ddc46aa82bad212fb38839547c54de7063
Imam Ali Mustofa <d.darkterminal@gmail.com>
2022-06-03 20:37:56 +0700

    chore: changed error 404 routing

--- backend/application/config/routes.php
+++ backend/application/config/routes.php
@@ -50,5 +50,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome/index';
-$route['404_override'] = '';
+$route['404_override'] = 'welcome/error_404';
 $route['translate_uri_dashes'] = TRUE;
