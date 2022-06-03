790b67dc545ac8f818bd0bdba9d9c7503e909a4f
Imam Ali Mustofa <d.darkterminal@gmail.com>
2022-06-03 20:37:34 +0700

    chore: added token middleware to Account Endpoint

--- backend/application/controllers/api/v1/Accounts.php
+++ backend/application/controllers/api/v1/Accounts.php
@@ -1,13 +1,12 @@
 <?php
-
 defined('BASEPATH') or exit('No direct script access allowed');
-
 class Accounts extends Rest_Controller
 {
 
     public function __construct()
     {
         parent::__construct();
+        token_middleware();
     }
 
     public function index_get()
